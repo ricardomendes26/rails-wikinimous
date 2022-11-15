0e3c538a87c81e3972383d93f698ef36d97ad558
ricardomendes <ricardogaditmendes26@gmail.com>
2022-11-15 18:58:38 +0000

    exercise3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+articles = Article.create
+10.times do
+    Article.create(title: Faker::Science.science, content: Faker::Space.planet)
+  end
